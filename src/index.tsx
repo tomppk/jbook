c2e8f3493deeb7338dd48cca43a96c678f0a2473
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-13 17:14:16 +0300

    implement esbuild service

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,38 @@
-import { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 // <pre> element formats code nicely and makes it look like code
 const App = () => {
+  // Initialize useRef hook with type
+  // Ref can be a reference to any javascript value inside a component, not only
+  // a reference to HTML DOM element. We will use ref.current to hold a reference
+  // to esbuild service object to access esbuild functionality inside our component
+  const ref = useRef<any>();
+
   // input is code that user writes into textarea
   // code is output from ESbuild tool, so transpiled and bundled codo that user
   //inputs. Show this code output in <pre> element
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+
+  // Initialize esbuild service with config object.
+  // wasmURL is path to esbuild webassembly binary in our public folder
+  // startService returns esbuild object with functions to transpile and bundle
+  // code
+  // Assign
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  // useEffect hook similar to componentDidMount class based method
+  // Calls startService() once when the App component is first rendered.
+  useEffect(() => {
+    startService();
+  }, []);
 
   // Set input to be the value of keypress inside text field
   const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
@@ -16,7 +41,11 @@ const App = () => {
 
   //
   const onClick = (): void => {
-    console.log(input);
+    if (!ref.current) {
+      return;
+    }
+
+    console.log(ref.current);
   };
 
   return (
