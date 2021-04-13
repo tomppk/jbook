5e3f9d8e6eb45348547d694dc68eedff833370e0
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-13 16:52:39 +0300

    create initial App component inside index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,32 @@
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
+// <pre> element formats code nicely and makes it look like code
 const App = () => {
-  return <h1>Hi!</h1>;
+  // input is code that user writes into textarea
+  // code is output from ESbuild tool, so transpiled and bundled codo that user
+  //inputs. Show this code output in <pre> element
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  // Set input to be the value of keypress inside text field
+  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    setInput(event.target.value);
+  };
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={onChange}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
