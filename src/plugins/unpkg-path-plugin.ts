f9e5c794cbbf9ef9220ae1d936a7ea93bd1fb713
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-16 13:59:01 +0300

    add axios and fetch tiny-test-pkg from unpkg.com api

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,4 +1,5 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
 
 // A plugin that works inside ESbuild
 // Similar plugins can be applied to other module bundlers as well like Webpack
@@ -12,6 +13,8 @@ export const unpkgPathPlugin = () => {
     // setup function called automatically
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // onResolve is called whenever ESbuild is trying to figure out a path
+      // to a particular module.
       // event hanlder to listen for onResolve event of build process
       // overrides the default process of esbuild of figuring out where to find
       // file
@@ -20,8 +23,15 @@ export const unpkgPathPlugin = () => {
       // namespace: is used to flag files we want onResolve and onLoad to be
       // applied to. Can have many files with different namespaces
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
+        console.log('onResolve', args);
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        } else if (args.path === 'tiny-test-pkg') {
+          return {
+            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+            namespace: 'a',
+          };
+        }
       });
       // event handler to listen for onLoad event of build process
       // overrides esbuilds default way of loading up a file which is to read it
@@ -34,15 +44,25 @@ export const unpkgPathPlugin = () => {
         // If esbuild tries to load index.js file do not let it do it and load
         // up something from file system but instead it is loaded here already
         // inside object
+        // loader property: 'jsx' informs ESbuild that contents might include
+        // 'jsx' and it knows then to parse it properly
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'tiny-test-pkg';
+              const message = require('tiny-test-pkg');
               console.log(message);
             `,
           };
         }
+        // Make GET request to unpkg.com and extract data property from response
+        // Return object with structure ESbuild can understand so loader to determine
+        // type of data or code and contents
+        const { data } = await axios.get(args.path);
+        return {
+          loader: 'jsx',
+          contents: data,
+        };
       });
     },
   };
