12947380bc7b18cb0244e1b5e9a7def345c80c12
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-17 18:33:56 +0300

    remove esbuild-wasm from public folder and fetch it from unpkg.com instead

--- src/index.tsx
+++ src/index.tsx
@@ -19,14 +19,13 @@ const App = () => {
   const [code, setCode] = useState('');
 
   // Initialize esbuild service with config object.
-  // wasmURL is path to esbuild webassembly binary in our public folder
+  // wasmURL is path to esbuild webassembly binary that we fetch from unpkg.com
   // startService returns esbuild object with functions to transpile and bundle
   // code
-  // Assign
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
 
