a9dc6b5faa5e8af963036f83c49cd909c7d70172
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-20 08:15:58 +0300

    add resizable handles css styling

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -23,14 +23,11 @@ const CodeCell = () => {
   // Pass down code state to Preview component
   return (
     <Resizable direction="vertical">
-      <div>
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <CodeEditor
           initialValue="const a = 1;"
           onChange={(value) => setInput(value)}
         />
-        <div>
-          <button onClick={onClick}>Submit</button>
-        </div>
         <Preview code={code} />
       </div>
     </Resizable>
--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import { ResizableBox } from 'react-resizable';
+import './resizable.css';
 
 // Interface to define required props for the component
 interface ResizableProps {
@@ -9,10 +10,13 @@ interface ResizableProps {
 // We always receive children props if we try to place some content or another
 // React component inside this component as props
 // By wrapping children with this component we can make them resizable.
-//
+// Configue ResizableBox properties according to npm documentation
+// width Infinity is same as 100% so as much space as possible
+// resizeHandles takes array with directions like south on which side to display
+// handles
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   return (
-    <ResizableBox height={300} width={300} resizeHandles={['s']}>
+    <ResizableBox height={300} width={Infinity} resizeHandles={['s']}>
       {children}
     </ResizableBox>
   );
