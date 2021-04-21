c7ac3f1406445024e9a8a948c335005022e4a28e
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-21 11:29:02 +0300

    create action creators

--- src/redux/action-creators/index.ts
+++ src/redux/action-creators/index.ts
@@ -1,1 +1,52 @@
-export {};
+import { ActionType } from '../action-types';
+import {
+  Action,
+  UpdateCellAction,
+  DeleteCellAction,
+  MoveCellAction,
+  InsertCellBeforeAction,
+  Direction,
+} from '../actions';
+import { CellTypes } from '../cell';
+
+// Annotate return types for specific actions
+// Take in properties, assign them to payload values and return action
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content,
+    },
+  };
+};
+
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  };
+};
+
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction,
+    },
+  };
+};
+
+export const insertCellBefore = (
+  id: string,
+  cellType: CellTypes
+): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type: cellType,
+    },
+  };
+};
--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,24 +1,27 @@
 import { ActionType } from '../action-types';
 import { CellTypes } from '../cell';
 
+// Define type for direction cells can move
+export type Direction = 'up' | 'down';
+
 // Define interfaces for each action
 // MoveCell id of cell we want to move, direction where to move
-interface MoveCellAction {
+export interface MoveCellAction {
   type: ActionType.MOVE_CELL;
   payload: {
     id: string;
-    direction: 'up' | 'down';
+    direction: Direction;
   };
 }
 
 // Payload id of cell we want to delete
-interface DeleteCellAction {
+export interface DeleteCellAction {
   type: ActionType.DELETE_CELL;
   payload: string;
 }
 
 // Id of cell and type of cell either code or text cell
-interface InsertCellBeforeAction {
+export interface InsertCellBeforeAction {
   type: ActionType.INSERT_CELL_BEFORE;
   payload: {
     id: string;
@@ -27,7 +30,7 @@ interface InsertCellBeforeAction {
 }
 
 // Id of cell we want to update. String of content either text or code
-interface UpdateCellAction {
+export interface UpdateCellAction {
   type: ActionType.UPDATE_CELL;
   payload: {
     id: string;
