53925261e70868e736d328c0d8c3b53d1557870c
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-21 10:05:29 +0300

    add action types and action interfaces

--- src/redux/action-types/index.ts
+++ src/redux/action-types/index.ts
@@ -1,1 +1,7 @@
-export {};
+// Enum defining all our action types
+export enum ActionType {
+  MOVE_CELL = 'move_cell',
+  DELETE_CELL = 'delete_cell',
+  INSERT_CELL_BEFORE = 'insert_cell_before',
+  UPDATE_CELL = 'update_cell',
+}
--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,1 +1,43 @@
-export {};
+import { ActionType } from '../action-types';
+
+// Define interfaces for each action
+// MoveCell id of cell we want to move, direction where to move
+interface MoveCellAction {
+  type: ActionType.MOVE_CELL;
+  payload: {
+    id: string;
+    direction: 'up' | 'down';
+  };
+}
+
+// Payload id of cell we want to delete
+interface DeleteCellAction {
+  type: ActionType.DELETE_CELL;
+  payload: string;
+}
+
+// Id of cell and type of cell either code or text cell
+interface InsertCellBeforeAction {
+  type: ActionType.INSERT_CELL_BEFORE;
+  payload: {
+    id: string;
+    type: 'code' | 'text';
+  };
+}
+
+// Id of cell we want to update. String of content either text or code
+interface UpdateCellAction {
+  type: ActionType.UPDATE_CELL;
+  payload: {
+    id: string;
+    content: string;
+  };
+}
+
+// Export Action which is a union of all our actions so contains all possible
+// actions for Typescript to check that only these actions are allowed
+export type Action =
+  | MoveCellAction
+  | DeleteCellAction
+  | InsertCellBeforeAction
+  | UpdateCellAction;
