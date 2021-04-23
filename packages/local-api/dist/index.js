2c509f46cfb2c5b7d9a58b7409aae11bea98e1ab
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-23 15:18:33 +0300

    add express server to local-api

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,9 +1,22 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
+var express_1 = __importDefault(require("express"));
 var serve = function (port, filename, dir) {
-    console.log('serving traffic on port', port);
-    console.log('saving/fetching cells from', filename);
-    console.log('that file is in dir', dir);
+    var app = express_1.default();
+    // Wrap starting express server listening inside a custom Promise to enable
+    // async error handling in CLI.
+    // The Promise will be resolved or rejected at some point in time.
+    // If we successfully start up our server and start listening then resolve
+    // function will be called automatically which resolves the Promise.
+    // If something goes wrong with starting express server reject function is
+    // called. That will reject Promise and put it in error state which will in
+    // turn throw an error at CLI serve command try/catch block.
+    return new Promise(function (resolve, reject) {
+        app.listen(port, resolve).on('error', reject);
+    });
 };
 exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,5 +1,17 @@
+import express from 'express';
+
 export const serve = (port: number, filename: string, dir: string) => {
-  console.log('serving traffic on port', port);
-  console.log('saving/fetching cells from', filename);
-  console.log('that file is in dir', dir);
+  const app = express();
+
+  // Wrap starting express server listening inside a custom Promise to enable
+  // async error handling in CLI.
+  // The Promise will be resolved or rejected at some point in time.
+  // If we successfully start up our server and start listening then resolve
+  // function will be called automatically which resolves the Promise.
+  // If something goes wrong with starting express server reject function is
+  // called. That will reject Promise and put it in error state which will in
+  // turn throw an error at CLI serve command try/catch block.
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on('error', reject);
+  });
 };
