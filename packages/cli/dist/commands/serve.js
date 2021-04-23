f80e53569da972a9ec27467ae7210ad398153cc2
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-23 14:33:39 +0300

    add calculate file paths to serve command in CLI

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -1,8 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
+// Path is part of Node standard library. Functions to help calculate different
+// paths on a file system
+var path_1 = __importDefault(require("path"));
 // Command() creates instance of command for us to implement
 var commander_1 = require("commander");
+var local_api_1 = require("local-api");
 // Create instance of Command() from commander library designed to help building
 // CLI tools. Chain on method calls to customize the Command object.
 // command('nameOfCommand') defines what command we want to watch for. When user
@@ -16,12 +23,25 @@ var commander_1 = require("commander");
 // so the actual logic of the command.
 // Action function receives as first argument optional value from .command
 // Second argument object containing different options
+// Execute serve(port, filename, directory) function defined in 'local-api'
+// package
+// process.cwd() returns absolute path to directory where user ran the
+// command from eg. home/projects/tbook/packages/cli
+// path.dirname(filename) returns directory name that is specified inside
+// filename string so if relative path specified returns folder name
+// eg. if input is notes/notebook.js returns notes/ or if just filename
+// inputted then returns empty string ''
+// These two directory paths are joined together to get final path
+// path.basename(filename) returns just filename from relative path
 exports.serveCommand = new commander_1.Command()
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p, --port <number>', 'port to run server on', '4005')
     .action(function (filename, options) {
     if (filename === void 0) { filename = 'notebook.js'; }
-    console.log(filename, options);
+    var dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
+    local_api_1.serve(parseInt(options.port), path_1.default.basename(filename), dir);
 });
 // To run command from command line node index.js serve
+// console.log(path.join(process.cwd(), path.dirname(filename)));
+// console.log(path.basename(filename));
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,5 +1,9 @@
+// Path is part of Node standard library. Functions to help calculate different
+// paths on a file system
+import path from 'path';
 // Command() creates instance of command for us to implement
 import { Command } from 'commander';
+import { serve } from 'local-api';
 
 // Create instance of Command() from commander library designed to help building
 // CLI tools. Chain on method calls to customize the Command object.
@@ -17,12 +21,28 @@ import { Command } from 'commander';
 // so the actual logic of the command.
 // Action function receives as first argument optional value from .command
 // Second argument object containing different options
+// Execute serve(port, filename, directory) function defined in 'local-api'
+// package
+
+// process.cwd() returns absolute path to directory where user ran the
+// command from eg. home/projects/tbook/packages/cli
+// path.dirname(filename) returns directory name that is specified inside
+// filename string so if relative path specified returns folder name
+// eg. if input is notes/notebook.js returns notes/ or if just filename
+// inputted then returns empty string ''
+// These two directory paths are joined together to get final path
+// path.basename(filename) returns just filename from relative path
+
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
-  .action((filename = 'notebook.js', options) => {
-    console.log(filename, options);
+  .action((filename = 'notebook.js', options: { port: string }) => {
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(options.port), path.basename(filename), dir);
   });
 
 // To run command from command line node index.js serve
+
+// console.log(path.join(process.cwd(), path.dirname(filename)));
+// console.log(path.basename(filename));
